69b30998f13169f6d993bbf8b7bb5132700ec1e4
SoonHyunAn <ash7106@gmail.com>
2024-04-12 15:11:02 +0900

    React_YouTube

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,30 +1,76 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { formatAgo } from "../util/date";
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import Card from '@mui/material/Card';
+import { CardContent } from "@mui/material";
+
 
 export default function VideoCard({ video }) {
+  const { data: url } = useQuery({
+    queryKey: ['channel', video],
+    queryFn: async () => {
+      return axios
+        .get('/data/channel.json')
+        .then(res => res.data.items[0].snippet.thumbnails.default.url)
+    }, staleTime: 1000 * 60 * 5,
+  }); 
+
   const titleStyle = {
-    padding: '0px 0px 0px 0px',
-    width: 250
+    width: 250,
+    overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'
   };
+
+  const cardStyle = {
+    border: 'none', maxHeight: 400, width: 300, margin: '5px 0',
+    '&:hover': {
+      cursor: 'pointer', // 마우스를 올렸을 때 커서를 클릭 모양으로 변경
+    },
+  }
 
   const navigate = useNavigate();
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
-  if (typeof (video.id) !== 'string' && video.id.kind === 'youtube#channel')
-    return;
+  // const [viewCount, setViewCount] = useState(null); // 조회수
+
+  // if (typeof (video.id) !== 'string' && video.id.kind === 'youtube#channel')
+  //   return;
+
   const videoId = typeof (video.id) === 'string' ? video.id : video.id.videoID;
+
+  // const urls = `https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoId}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`;
+
+  // axios.get(urls)
+  //   .then(response => {
+  //     setViewCount(response.data.items[0].statistics.viewCount);
+  //   })
+  //   .catch(error => {
+  //     console.error('API 호출 오류:', error);
+  //   });
+
+
+
   return (
-    <Card width={250} onClick={() => {
+    <Card width={250} variant="outlined" sx={cardStyle} onClick={() => {
       navigate(`/videos/watch/${videoId}`, { state: { video } })
     }}>
-
-      <img src={thumbnails.medium.url} alt={title} width={250} />
-      <div>
-        <p style={titleStyle}>{title}</p>
-        <p style={titleStyle}>{channelTitle}</p>
-        <p style={titleStyle}>{formatAgo(publishedAt, 'ko')}</p>
-      </div>
+      <CardContent style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+        <img src={thumbnails.medium.url} alt={title} width={300} />
+        <div style={{ flex: 1, justifyContent: "center", display: 'flex' }}>
+          {url && <img src={url} alt={""} style={{ width: 30, height: 30, marginTop: 10, marginRight: 8 }} />}
+          <p style={{ ...titleStyle, fontWeight: 'bolder' }}>{title}</p>
+        </div>
+        <div>
+          <p style={titleStyle}>{channelTitle}</p>
+        </div>
+        <div>
+          <p style={titleStyle}>{formatAgo(publishedAt, 'ko')}</p>
+          {/* <p>총 조회수: {viewCount}</p> */}
+        </div>
+      </CardContent>
     </Card>
   )
 }
+
+
+// 주석 부분은 조회수 받아오는 것 - api 녹으니 주의
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,45 +1,50 @@
 import React from "react";
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import { useQuery } from '@tanstack/react-query';
+// import { useQuery } from '@tanstack/react-query';
+// import axios from 'axios';
+import Grid from "@mui/material/Grid";
 import HourglassTopIcon from '@mui/icons-material/HourglassTop';
 import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 import VideoCard from '../components/VideoCard';
 
+import { useVideo } from '../api/youtube';
+
+// const keywordUri = `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_YOUTUBE_API_KEY}&maxResults=25&part=snippet&q=`;
+// const popularUri = `https://youtube.googleapis.com/youtube/v3/videos?chart=mostPopular&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&maxResults=25&part=snippet`;
 
 export default function Videos() {
   const { keyword } = useParams();
-  const keywordUri = `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_YOUTUBE_API_KEY}&maxResults=25&part=snippet&q=${keyword}`;
-  const popularUri = `https://youtube.googleapis.com/youtube/v3/videos?chart=mostPopular&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&maxResults=25&part=snippet`;
-  const { isLoading, error, data: videos } = useQuery({
-    queryKey: ['videos', keyword],
-    queryFn: async () => {
-      const uri = keyword ? keywordUri + keyword : popularUri;
-      return axios
-        // .get(`/data/${keyword ? 'search' : 'popular'}.json`)
-        .get(uri)
-        .then(res => res.data.items);
-    }, staleTime: 1000 * 60 * 1,
-  });
-  // useEffect(() => {
-  //   axios.get(`/data/${keyword ? 'search' : 'popular'}.json`)
-  //     .then(res => setVideos(res.data.items))
-  // }, [keyword])
+
+  const { isLoading, error, videos } = useVideo(keyword);
+
+  // 수정 전 버전 - youtube.js 사용 전
+
+  // const { isLoading, error, data: videos } = useQuery({
+  //   queryKey: ['videos', keyword],
+  //   queryFn: async () => {
+  //     const uri = keyword ? keywordUri + keyword : popularUri;
+  //     return axios
+  //       .get(`/data/${keyword ? 'search' : 'popular'}.json`)
+  //       // .get(uri)
+  //       .then(res => res.data.items);
+  //   }, staleTime: 1000 * 60 * 1,
+  // });
+
   return (
-    <div>
+    <>
       <div style={{ marginBottom: '10px', color: "lightcoral", padding: '0px 0px 15px 25px' }}>Videos {keyword ? `${keyword} 검색` : 'Hot Trend'}</div>
       {isLoading && <p><HourglassTopIcon />Loading</p>}
       {error && <p><WarningAmberIcon />Something is wrong!!!</p>}
       {videos && (
-        <div style={{ textAlign: 'left', display: 'flex', flexDirection: 'row', alignItems: 'flex-start', flexWrap: 'wrap', gap: '1px', margin: '5px' }}>
+        <Grid container spacing={1} style={{ textAlign: 'left', display: 'flex', flexDirection: 'row', alignItems: 'flex-start', flexWrap: 'wrap', gap: '1px', margin: '5px' }}>
           {videos.map((video, index) => (
-            <div key={index} style={{ flex: 1, marginBottom: '1px', textAlign: 'left' }}>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
               <VideoCard video={video} />
               <div>{video.title}</div>
-            </div>
+            </Grid>
           ))}
-        </div>
+        </Grid>
       )}
-    </div>
+    </>
   )
 }
